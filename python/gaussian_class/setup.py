02737c6ab5c29c026a990179a03ca4792b87c620
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-01-31 23:20:04 +0300

    setup information for the package

--- python/gaussian_class/setup.py
+++ python/gaussian_class/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name="distributions", 
+setup(name="gaussian_and_binomial_distributions", 
  version="0.1",
  description="Gaussian distributions", 
- packages=['distributions'],
+ packages=['gaussian_and_binomial_distributions'],
  zip_safe=False)
